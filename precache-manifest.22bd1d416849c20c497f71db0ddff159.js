d9ce572089db3eff3f3b48a521dedffa5fd98402
anjitzero <anjitpariyar@gmail.com>
2020-08-08 10:59:27 +0545

    Updates

---
+++ precache-manifest.22bd1d416849c20c497f71db0ddff159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd0ced736939cdade181bb1396f8e1b",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "3c74d9ae59b1774d1789",
+    "url": "/contact-manager/static/css/main.fc4a27a2.chunk.css"
+  },
+  {
+    "revision": "29ff548c1a7b36fa232a",
+    "url": "/contact-manager/static/js/2.c3020317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact-manager/static/js/2.c3020317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c74d9ae59b1774d1789",
+    "url": "/contact-manager/static/js/main.257342ff.chunk.js"
+  },
+  {
+    "revision": "317da0919aefa8a66f4a",
+    "url": "/contact-manager/static/js/runtime-main.69c414c1.js"
+  }
+]);
